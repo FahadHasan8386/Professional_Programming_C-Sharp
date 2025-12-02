debefb9a5835b19738cb4d80e1b68263ad19d232
Fahad Hasan <hasan.fahad67@outlook.com>
2025-12-02 12:04:20 +0600

    Problem Solve

---
+++ cf_1697B/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var first = Console.ReadLine().Split();
+        int n = int.Parse(first[0]);
+        int q = int.Parse(first[1]);
+
+        long[] v = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+        Array.Sort(v);
+
+        long[] s = new long[n + 1];
+
+        for (int i = 0; i < n; i++)
+        {
+            s[i + 1] = s[i] + v[i];
+        }
+
+        while (q-- > 0)
+        {
+            var parts = Console.ReadLine().Split();
+            int x = int.Parse(parts[0]);
+            int y = int.Parse(parts[1]);
+
+            long result = s[n - x + y] - s[n - x];
+
+            Console.WriteLine(result);
+
+        }
+
+    }
+}
---
+++ cf_75A/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static long RemoveZero(long num)
+    {
+        string s = num.ToString();
+        string t = "";
+
+        foreach (char c in s)
+        {
+            if (c != '0')
+                t += c;
+        }
+
+        return long.Parse(t);
+    }
+
+    static void Main()
+    {
+        string[] input_number = Console.ReadLine().Split();
+
+        long a = long.Parse(input_number[0]);
+        long b = long.Parse(input_number[1]);
+
+        long sum = a + b;
+
+        long aClean = RemoveZero(a);
+        long bClean = RemoveZero(b);
+        long sumClean = RemoveZero(sum);
+
+        if (aClean + bClean == sumClean)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
