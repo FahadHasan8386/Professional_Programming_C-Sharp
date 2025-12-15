dc2a973cd6f6855ecd1f556aa1331dcf8d3864df
Fahad Hasan <hasan.fahad67@outlook.com>
2025-12-15 20:12:02 +0600

    Constant Readonly Static

---
+++ Constant_Readonly/Example.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Constant_Readonly
+{
+    public class Example
+    {
+        // Compile-time Constant
+        public const int Num = 0;
+    }
+
+    // Runtime Constant
+    public class Example2
+    {
+        public static readonly string Name = "Fahad Hasan";
+    }
+
+    // Static readonly with Constructor
+    class Config
+    {
+        public static readonly DateTime AppStartTime;
+
+        static Config()
+        {
+            AppStartTime = DateTime.Now;
+        }
+    }
+
+    
+}
---
+++ Constant_Readonly/Program.cs
@@ -0,0 +1,18 @@
+﻿using Constant_Readonly;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // const usage
+        Console.WriteLine("Const Num: " + Example.Num);
+
+        // static readonly usage
+        Console.WriteLine("Readonly Name: " + Example2.Name);
+
+        // static readonly with constructor
+        Console.WriteLine("App Start Time: " + Config.AppStartTime);
+
+        Console.ReadKey();
+    }
+}
