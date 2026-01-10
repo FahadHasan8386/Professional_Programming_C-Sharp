ea0d157b05c602157211a0d56968e3f92d9228a0
Fahad Hasan <hasan.fahad67@outlook.com>
2026-01-10 19:09:52 +0600

    Async,Await

---
+++ async_await_delay/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Program Started");
+
+        await DoWorkAsync();
+
+        Console.WriteLine("Program Ended");
+    }
+
+    static  async Task DoWorkAsync()
+    {
+        Console.WriteLine("Work Started");
+
+        await Task.Delay(3000);
+
+        Console.WriteLine("Word Ended");
+    }
+}
