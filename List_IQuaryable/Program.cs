a29ad969dcc0f1055acbd0cf69a673b7f8e49648
Fahad Hasan <hasan.fahad67@outlook.com>
2026-01-11 19:35:49 +0600

    List IEnumrable IQueryable

---
+++ List_IQuaryable/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //Save on ram (Memory)
+        List<int> numbers = new List<int> { 1 , 2, 3 };
+
+        //IEnumrable
+        IEnumerable<int> EvenNumberable = numbers.Where(n => n % 2 == 0);
+
+        //IQueryable
+        IQueryable<int> evenNumberQuery =numbers.AsQueryable().Where(n => n % 2 == 0);
+
+        numbers.Add(1);
+
+        Console.WriteLine("IEnumrable Result");
+        foreach(var n in EvenNumberable)
+        {
+            Console.WriteLine(n);
+        }
+
+        Console.WriteLine("IQueryrable Result");
+        foreach (var n in evenNumberQuery)
+        {
+            Console.WriteLine(n);
+        }
+
+    }
+}
