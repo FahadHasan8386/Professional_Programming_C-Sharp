a84a29208bee3f61f6740012bb46e480c3efcd19
Fahad Hasan <hasan.fahad67@outlook.com>
2026-01-12 20:49:36 +0600

    Student App Info

---
+++ Student_App_Info/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Student_App_Info
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+              "Server=.;Database=StudentDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Student_App_Info/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+
+public class StudentAdoService
+{
+    string cs = "Server=.;Database=StudentDB;Trusted_Connection=True;";
+
+    public async Task<List<Student>> GetStudents()
+    {
+        List<Student> list = new List<Student>();
+
+        SqlConnection con = new SqlConnection(cs);
+        await con.OpenAsync();
+
+        SqlCommand cmd = new SqlCommand(
+            "SELECT * FROM Students", con);
+
+        SqlDataReader rd = await cmd.ExecuteReaderAsync();
+
+        while (await rd.ReadAsync())
+        {
+            list.Add(new Student
+            {
+                Id = (int)rd["Id"],
+                Name = rd["Name"].ToString(),
+                Age = (int)rd["Age"]
+            });
+        }
+
+        con.Close();
+        return list;
+    }
+}
