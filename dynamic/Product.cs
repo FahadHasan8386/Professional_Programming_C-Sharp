1ba486945bc008c701aa07d160031f8f6e951259
Fahad Hasan <hasan.fahad67@outlook.com>
2026-02-25 09:46:14 +0600

    Dynamic

---
+++ dynamic/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dynamic
+{
+    public class Product
+    {
+        public string Brand {  get; set; }
+        public double Price { get; set; }
+    }
+}
--- dynamic/Program.cs
+++ dynamic/Program.cs
@@ -1,5 +1,27 @@
-﻿dynamic person = new { Name = "Fahad", Age = 44 };
+﻿using dynamic;
+
+dynamic person = new { Name = "Fahad", Age = 44 };
 
 Console.WriteLine(person.Name);
 
 Console.WriteLine(person.Name2);
+
+
+dynamic product = new Product { Brand = "Camera", Price = 100 };
+
+//compile time e kichu hobe nh ,Run time e error dibe shob 
+Console.WriteLine(product.Rating);
+product.CalculateDiscountPrice(); 
+
+
+//limited case e use hoi ,runtime ,r c# Compile type 
+
+//like this use case 
+
+object MyMethod(int x)
+{
+
+}
+//amra just jni obeject er vitor akta name property ase 
+dynamic myMethod = MyMethod(4);
+string name = myMethod.Name;
